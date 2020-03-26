ba49c4c225c3fbcdc38a6e6db32c8b2623ff4335
Markus Merklinger <markus.merklinger@code.berlin>
2020-03-26 14:58:10 +0100

    Added config module, which parses configuration either from .env file or environment variables

---
+++ src/api/config.rs
@@ -0,0 +1,57 @@
+use dotenv::dotenv;
+use std::env;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use log::debug;
+
+
+const HTTP_HOST_DEFAULT: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+const HTTP_PORT_DEFAULT: u16 = 8080;
+
+pub struct Config {
+    http_host: Option<IpAddr>,
+    http_port: Option<u16>,
+}
+
+pub enum Error {
+    HttpHostInvalidValue,
+    HttpPortInvalidValue,
+}
+
+impl Config {
+    pub fn new() -> Result<Config, Error> {
+        dotenv().ok();
+
+        let mut config = Config {
+            http_host: Some(HTTP_HOST_DEFAULT),
+            http_port: Some(HTTP_PORT_DEFAULT),
+        };
+
+        // HTTP_HOST
+        match env::var("HTTP_HOST") {
+            Ok(val) => {
+                match val.parse::<Ipv4Addr>() {
+                    Ok(val) => config.http_host = Some(IpAddr::V4(val)),
+                    Err(_) => return Err(Error::HttpHostInvalidValue)
+                }
+            }
+            Err(_) => debug!("'HTTP_HOST' not set or not valid unicode, using default value")
+        }
+
+        // HTTP_PORT
+        match env::var("HTTP_PORT") {
+            Ok(val) => {
+                match val.parse::<u16>() {
+                    Ok(val) => config.http_port = Some(val),
+                    Err(_) => return Err(Error::HttpPortInvalidValue)
+                }
+            }
+            Err(_) => debug!("'HTTP_PORT' not set or not valid unicode, using default value")
+        }
+
+        return Ok(config);
+    }
+
+    pub fn http_socket(&self) -> SocketAddr {
+        SocketAddr::new(self.http_host.unwrap(), self.http_port.unwrap())
+    }
+}
--- src/api/main.rs
+++ src/api/main.rs
@@ -1,12 +1,27 @@
 use warp::Filter;
+use log::error;
+use std::process;
 
 mod router;
 mod endpoints;
+mod config;
 
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
+
+    let config = match config::Config::new() {
+        Ok(val) => val,
+        Err(config::Error::HttpHostInvalidValue) => {
+            error!("Terminating, due to invalid 'HTTP_HOST' environment variable");
+            process::exit(1);
+        }
+        Err(config::Error::HttpPortInvalidValue) => {
+            error!("Terminating, due to invalid 'HTTP_PORT' environment variable");
+            process::exit(1);
+        }
+    };
 
     let routes = router::root()
         .or(router::identity())
@@ -15,6 +30,6 @@ async fn main() {
         .or(router::notification());
 
     warp::serve(routes)
-        .try_bind(([127, 0, 0, 1], 8080))
+        .try_bind(config.http_socket())
         .await;
 }
