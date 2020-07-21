6a0cd1c433c6d2bc5fe9a41abe04427bd7f9bf73
Markus Merklinger <markus.merklinger@code.berlin>
2020-07-21 10:46:57 +0200

    Add logging dependencies and service start logging

--- notification/src/main.rs
+++ notification/src/main.rs
@@ -1,3 +1,11 @@
+extern crate log;
+
+
+static PKG_NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
+static PKG_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    println!("SERVICE: {} | VERSION: {}\n", PKG_NAME.unwrap_or("<unknown>"), PKG_VERSION.unwrap_or("<unknown>"));
+    env_logger::init();
+    log::info!("Starting service");
 }
