2f3418ecc12b967dc86fc6c20972e7b81e0357cc
Markus Merklinger <markus.merklinger@code.berlin>
2020-07-21 15:07:33 +0200

    Update main function to async

--- identity/src/main.rs
+++ identity/src/main.rs
@@ -3,7 +3,8 @@ extern crate log;
 static PKG_NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
 static PKG_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!(
         "SERVICE: {} | VERSION: {}\n",
         PKG_NAME.unwrap_or("<unknown>"),
