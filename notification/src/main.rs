b335d708eac1dfff9eca37322f98cba5d61b02d8
code-mm <43041831+code-mm@users.noreply.github.com>
2020-07-21 12:30:33 +0200

    Add configuration dependencies and static configuration data structure (#58)

--- notification/src/main.rs
+++ notification/src/main.rs
@@ -1,7 +1,26 @@
 extern crate log;
+use dotenv::dotenv;
+use envconfig::Envconfig;
+use lazy_static::lazy_static;
+use std::process;
+
+mod config;
 
 static PKG_NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
 static PKG_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+
+lazy_static! {
+    static ref CONFIGURATION: config::Config = {
+        dotenv().ok();
+        match config::Config::init() {
+            Ok(val) => val,
+            Err(e) => {
+                log::error!("{}", e);
+                process::exit(1);
+            }
+        }
+    };
+}
 
 fn main() {
     println!(
