7e310f2ce7a9b7d8db36bde9debb582e198b571b
Markus Merklinger <markus.merklinger@code.berlin>
2020-08-07 12:15:00 +0200

    Replace lazy_static with once_cell library

--- identity/src/main.rs
+++ identity/src/main.rs
@@ -1,38 +1,19 @@
 extern crate log;
 use dotenv::dotenv;
 use envconfig::Envconfig;
-use lazy_static::lazy_static;
 use std::process;
 extern crate diesel;
+use config::Configuration;
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool};
+use once_cell::sync::OnceCell;
 
 mod config;
 
 static PKG_NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
 static PKG_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
-
-lazy_static! {
-    static ref CONFIGURATION: config::Configuration = {
-        dotenv().ok();
-        match config::Configuration::init() {
-            Ok(val) => val,
-            Err(e) => {
-                log::error!("{}", e);
-                process::exit(1);
-            }
-        }
-    };
-    static ref DB_POOL: Pool<ConnectionManager<PgConnection>> = {
-        match Pool::new(ConnectionManager::new(CONFIGURATION.db_connection_url())) {
-            Ok(val) => val,
-            Err(e) => {
-                log::error!("{}", e);
-                process::exit(1);
-            }
-        }
-    };
-}
+static CONFIGURATION: OnceCell<Configuration> = OnceCell::new();
+static DB: OnceCell<Pool<ConnectionManager<PgConnection>>> = OnceCell::new();
 
 #[tokio::main]
 async fn main() {
@@ -43,4 +24,45 @@ async fn main() {
     );
     env_logger::init();
     log::info!("Starting service");
+
+    let configuration = {
+        dotenv().ok();
+        match Configuration::init() {
+            Ok(val) => val,
+            Err(e) => {
+                log::error!("{}", e);
+                log::error!("Terminating because of previous error.");
+                process::exit(1);
+            }
+        }
+    };
+    match CONFIGURATION.set(configuration) {
+        Ok(()) => log::info!("Successfully initialized service configuration"),
+        Err(_) => {
+            log::error!("Failed to provide global service configuration");
+            log::error!("Terminating because of previous error.");
+            process::exit(1);
+        }
+    }
+
+    let db: Pool<ConnectionManager<PgConnection>> = {
+        match Pool::new(ConnectionManager::new(
+            CONFIGURATION.get().unwrap().db_connection_url(),
+        )) {
+            Ok(val) => val,
+            Err(e) => {
+                log::error!("{}", e);
+                log::error!("Terminating because of previous error.");
+                process::exit(1);
+            }
+        }
+    };
+    match DB.set(db) {
+        Ok(()) => log::info!("Successfully initialized database connection"),
+        Err(_) => {
+            log::error!("Failed to provide global database connection");
+            log::error!("Terminating because of previous error.");
+            process::exit(1);
+        }
+    }
 }
