21b52d266686f117ba498a388dce1d1ef9f5f4a9
Markus Merklinger <markus.merklinger@code.berlin>
2020-09-04 13:11:38 +0200

    Add rpc identity configuration variables

--- api/src/config.rs
+++ api/src/config.rs
@@ -9,10 +9,20 @@ pub struct Configuration {
 
     #[envconfig(from = "HTTP_HOST_PORT", default = "8080")]
     http_host_port: u16,
+
+    #[envconfig(from = "IDENTITY_SERVICE_HOST_IP", default = "127.0.0.1")]
+    identity_service_host_ip: IpAddr,
+
+    #[envconfig(from = "IDENTITY_SERVICE_HOST_PORT", default = "8081")]
+    identity_service_host_port: u16,
 }
 
 impl Configuration {
     pub fn http_socket(&self) -> SocketAddr {
         SocketAddr::new(self.http_host_ip, self.http_host_port)
+    }
+
+    pub fn identity_service_socket(&self) -> SocketAddr {
+        SocketAddr::new(self.identity_service_host_ip, self.identity_service_host_port)
     }
 }
