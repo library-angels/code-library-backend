9f5bff736f6f61a4ee2454d0c5ed03fcf9abc72d
Markus Merklinger <markus.merklinger@code.berlin>
2020-09-10 00:29:26 +0200

    Add rest endpoint oauth client identifier to api service

--- api/src/router.rs
+++ api/src/router.rs
@@ -1,8 +1,23 @@
+use crate::middleware::rejection;
 use warp::{filters::BoxedFilter, Filter, Reply};
 
 pub fn router() -> BoxedFilter<(impl Reply,)> {
     warp::path::end()
         .and(warp::get())
         .and_then(crate::endpoints::root::root)
+        .boxed()
+        .or(warp::path("identity").and(
+            warp::path("oauth")
+                .and(
+                    // GET - /identity/oauth/client_identifier
+                    warp::path("client_identifier")
+                        .and(warp::path::end())
+                        .and(warp::get())
+                        .and_then(crate::endpoints::identity::get_oauth_client_identifier)
+                        .boxed()
+                )
+                .boxed()
+        ))
+        .recover(rejection::handle_rejection)
         .boxed()
 }
