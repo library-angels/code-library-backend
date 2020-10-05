718ec8f95a9c1b3740deccbd219c5779079995da
Urhengulas <johann.hemmann@code.berlin>
2020-10-05 15:44:15 +0200

    api::router: Restucture for better extensibility
    * remove some .boxed() methods
        -> only needed for returned filter
    * split router by path

--- api/src/router.rs
+++ api/src/router.rs
@@ -1,41 +1,36 @@
+use crate::endpoints::{identity, root};
 use crate::middleware;
+
 use warp::{filters::BoxedFilter, Filter, Reply};
 
 pub fn router() -> BoxedFilter<(impl Reply,)> {
-    warp::path::end()
-        .and(warp::get())
-        .and_then(crate::endpoints::root::root)
-        .boxed()
-        .or(warp::path("identity").and(
-            warp::path("oauth")
-                .and(
-                    // GET - /identity/oauth/client_identifier
-                    warp::path("client_identifier")
+    // GET - /
+    let root = warp::path::end().and(warp::get()).and_then(root::root);
+
+    let identity = warp::path("identity").and(
+        warp::path("oauth")
+            .and(
+                // GET - /identity/oauth/client_identifier
+                warp::path("client_identifier")
+                    .and(warp::path::end())
+                    .and(warp::get())
+                    .and_then(identity::get_oauth_client_identifier)
+                    // POST - /identity/oauth/authentication
+                    .or(warp::path("authentication")
                         .and(warp::path::end())
-                        .and(warp::get())
-                        .and_then(crate::endpoints::identity::get_oauth_client_identifier)
-                        .boxed()
-                        // POST - /identity/oauth/authentication
-                        .or(warp::path("authentication")
-                            .and(warp::path::end())
-                            .and(warp::post())
-                            .and(warp::body::json())
-                            .and_then(crate::endpoints::identity::create_oauth_authentication)
-                            .boxed()),
-                )
-                .boxed()
-                .or(warp::path("session")
-                    .and(
-                        // GET - /identity/session/info
-                        warp::path("info")
-                            .and(warp::path::end())
-                            .and(warp::get())
-                            .and(middleware::session::authorization())
-                            .and_then(crate::endpoints::identity::get_session_info)
-                            .boxed(),
-                    )
-                    .boxed()),
-        ))
+                        .and(warp::post())
+                        .and(warp::body::json())
+                        .and_then(identity::create_oauth_authentication)),
+            )
+            // GET - /identity/session/info
+            .or(warp::path!("session" / "info")
+                .and(warp::path::end())
+                .and(warp::get())
+                .and(middleware::session::authorization())
+                .and_then(identity::get_session_info)),
+    );
+
+    root.or(identity)
         .recover(middleware::rejection::handle_rejection)
         .boxed()
 }
