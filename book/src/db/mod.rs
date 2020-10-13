067fbbd9a39317c4967b1f190126a4972264352c
Urhengulas <johann.hemmann@code.berlin>
2020-10-13 17:26:13 +0200

    book: Query book by id (#95)
    * `db`:
        * add `fn conn() -> PooledConnection<ConnectionManager<PgConnection>>`
        * `queries`: queries for book and related data
    * `rpc`:
        * `models`: `struct Book` + constructor
        * `server`: error handling for query

--- book/src/db/mod.rs
+++ book/src/db/mod.rs
@@ -1,2 +1,16 @@
+use diesel::r2d2::{ConnectionManager, PooledConnection};
+use diesel::PgConnection;
+
 pub mod models;
+pub mod queries;
 pub mod schema;
+
+pub fn get_conn() -> PooledConnection<ConnectionManager<PgConnection>> {
+    match crate::DB.get() {
+        Some(pool) => match pool.get() {
+            Ok(conn) => conn,
+            Err(e) => panic!("Failed to get database connection: {}", e),
+        },
+        None => panic!("No database connection set"),
+    }
+}
--- book/src/rpc/server.rs
+++ book/src/rpc/server.rs
@@ -1,15 +1,26 @@
+use diesel::result::Error as DBError;
 use std::net::SocketAddr;
 use tarpc::context;
 
 use super::models::{Book, Error};
 use super::service::BookService;
+use crate::db::queries;
 
 #[derive(Clone)]
 pub struct BookServer(pub SocketAddr);
 
 #[tarpc::server]
 impl BookService for BookServer {
-    async fn get_book(self, _: context::Context, _book_id: u32) -> Result<Book, Error> {
-        unimplemented!();
+    async fn get_book(self, _: context::Context, book_id: u32) -> Result<Book, Error> {
+        match queries::get_book_by_id(book_id as i32) {
+            Ok(book) => Ok(book),
+            Err(e) => {
+                log::debug!("Error (BookServer::get_book): {}", e);
+                match e {
+                    DBError::NotFound => Err(Error::NotFound),
+                    _ => Err(Error::InternalError),
+                }
+            }
+        }
     }
 }
