672cf69cadd64a230b5ed90d0ceb1e55e9b50a66
Urhengulas <johann.hemmann@code.berlin>
2020-10-13 17:26:13 +0200

    book: Query book by id (#95)
    * `db`:
        * add `fn conn() -> PooledConnection<ConnectionManager<PgConnection>>`
        * `queries`: queries for book and related data
    * `rpc`:
        * `models`: `struct Book` + constructor
        * `server`: error handling for query

---
+++ book/src/db/queries.rs
@@ -0,0 +1,49 @@
+use diesel::prelude::*;
+
+use super::schema::*;
+use super::{get_conn, models as db_models};
+use crate::rpc::models as rpc_models;
+
+pub fn get_book_by_id(book_id: i32) -> QueryResult<rpc_models::Book> {
+    let (raw_book, category, language, publisher, series) = books::table
+        .find(book_id)
+        .inner_join(categories::table)
+        .inner_join(languages::table)
+        .inner_join(publishers::table)
+        .inner_join(series::table)
+        .get_result::<(
+            db_models::Book,
+            db_models::Category,
+            db_models::Language,
+            db_models::Publisher,
+            db_models::Series,
+        )>(&get_conn())?;
+    let authors = get_book_authors(book_id)?;
+    let subject_areas = get_book_subject_areas(book_id)?;
+
+    Ok(rpc_models::Book::new(
+        raw_book,
+        category,
+        language,
+        publisher,
+        series,
+        authors,
+        subject_areas,
+    ))
+}
+
+pub fn get_book_authors(book_id: i32) -> QueryResult<Vec<db_models::Person>> {
+    books_authors::table
+        .filter(books_authors::book_id.eq(book_id))
+        .inner_join(persons::table)
+        .select(persons::all_columns)
+        .get_results::<db_models::Person>(&get_conn())
+}
+
+pub fn get_book_subject_areas(book_id: i32) -> QueryResult<Vec<db_models::SubjectArea>> {
+    books_subject_areas::table
+        .filter(books_subject_areas::book_id.eq(book_id))
+        .inner_join(subject_areas::table)
+        .select(subject_areas::all_columns)
+        .get_results::<db_models::SubjectArea>(&get_conn())
+}
--- book/src/rpc/models.rs
+++ book/src/rpc/models.rs
@@ -1,4 +1,7 @@
+use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
+
+use crate::db::models as db_models;
 
 /// Error type returned to API
 pub use helpers::rpc::Error;
@@ -6,4 +9,51 @@ pub use helpers::rpc::Error;
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Book {
     pub id: i32,
+    /// E.g. SE20, STS5
+    pub code_identifier: String,
+    /// International Standard Book Number (https://en.wikipedia.org/wiki/International_Standard_Book_Number)
+    pub isbn: String,
+    /// International Standard Serial Number (https://en.wikipedia.org/wiki/International_Standard_Serial_Number)
+    pub issn: Option<String>,
+    pub release_date: NaiveDate,
+    pub subtitle: Option<String>,
+    pub title: String,
+
+    // one-to-many
+    pub category: db_models::Category,
+    pub language: db_models::Language,
+    pub publisher: db_models::Publisher,
+    pub series: db_models::Series,
+
+    // many-to-many
+    pub authors: Vec<db_models::Person>,
+    pub subject_areas: Vec<db_models::SubjectArea>,
+}
+
+impl Book {
+    pub fn new(
+        raw_book: db_models::Book,
+        category: db_models::Category,
+        language: db_models::Language,
+        publisher: db_models::Publisher,
+        series: db_models::Series,
+        authors: Vec<db_models::Person>,
+        subject_areas: Vec<db_models::SubjectArea>,
+    ) -> Self {
+        Self {
+            id: raw_book.id,
+            code_identifier: raw_book.code_identifier,
+            isbn: raw_book.isbn,
+            issn: raw_book.issn,
+            release_date: raw_book.release_date,
+            subtitle: raw_book.subtitle,
+            title: raw_book.title,
+            category,
+            language,
+            publisher,
+            series,
+            authors,
+            subject_areas,
+        }
+    }
 }
