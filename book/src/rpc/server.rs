e831995752b81e342953b8c726c8afc202f133c6
Urhengulas <johann.hemmann@code.berlin>
2020-11-17 10:43:15 +0100

    `book`: Move book assembling from `db::queries` to `rpc::server` (#144)

--- book/src/rpc/server.rs
+++ book/src/rpc/server.rs
@@ -11,6 +11,18 @@ pub struct BookServer(pub SocketAddr);
 #[tarpc::server]
 impl BookService for BookServer {
     async fn get_book(self, _: context::Context, book_id: u32) -> RpcResult<Book> {
-        Ok(queries::get_book_by_id(book_id as i32)?)
+        let book_id = book_id as i32;
+
+        let (raw_book, category, language, publisher) = queries::get_book_by_id(book_id)?;
+
+        Ok(Book::new(
+            raw_book,
+            category,
+            language,
+            publisher,
+            queries::get_book_series(book_id)?,
+            queries::get_book_authors(book_id)?,
+            queries::get_book_subject_areas(book_id)?,
+        ))
     }
 }
