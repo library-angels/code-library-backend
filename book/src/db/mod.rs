e6edd318b9c33e85f9d7620561d01f4e67e4574a
Urhengulas <johann.hemmann@code.berlin>
2020-11-23 10:53:54 +0100

    `book`: Mv `DB` into `db::mod` (#154)

--- book/src/db/mod.rs
+++ book/src/db/mod.rs
@@ -2,12 +2,14 @@ pub mod models;
 pub mod queries;
 pub mod schema;
 
-use diesel::r2d2::{ConnectionManager, PooledConnection};
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 use diesel::PgConnection;
+use once_cell::sync::OnceCell;
+
+pub static DB: OnceCell<Pool<ConnectionManager<PgConnection>>> = OnceCell::new();
 
 pub fn get_conn() -> PooledConnection<ConnectionManager<PgConnection>> {
-    crate::DB
-        .get()
+    DB.get()
         .expect("No database connection set")
         .get()
         .expect("Failed to get database connection")
