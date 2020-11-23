d72dc9c2529071271f6bf243c136a7dff6a58f8b
Urhengulas <johann.hemmann@code.berlin>
2020-11-23 10:53:54 +0100

    `book`: Mv `DB` into `db::mod` (#154)

--- book/src/lib.rs
+++ book/src/lib.rs
@@ -1,13 +1,8 @@
 mod db;
 mod rpc;
 
-use diesel::prelude::*;
-use diesel::r2d2::{ConnectionManager, Pool};
-use once_cell::sync::OnceCell;
-
 #[macro_use]
 extern crate diesel;
 
+pub use crate::db::DB;
 pub use crate::rpc::*;
-
-pub static DB: OnceCell<Pool<ConnectionManager<PgConnection>>> = OnceCell::new();
