6110642391882d28bc9b8a4ce21552f42deaf04f
Urhengulas <johann.hemmann@code.berlin>
2020-11-24 18:40:22 +0100

    `book::migrations`: More `UNIQUE` constraints

--- book/migrations/2020-10-12-164409_create_books/up.sql
+++ book/migrations/2020-10-12-164409_create_books/up.sql
@@ -10,38 +10,39 @@ CREATE TABLE persons (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    isni VARCHAR(16),
-    orcid VARCHAR(16),
-    oclc INTEGER
+    isni VARCHAR(16) UNIQUE,
+    orcid VARCHAR(16) UNIQUE,
+    oclc INTEGER UNIQUE
 );
 
 CREATE TABLE publishers (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE series (
     id SERIAL PRIMARY KEY,
     publisher_id INTEGER NOT NULL REFERENCES publishers (id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    UNIQUE(publisher_id, name)
 );
 
 CREATE TABLE languages (
     id SERIAL PRIMARY KEY,
-    iso_code VARCHAR(2) NOT NULL,
+    iso_code VARCHAR(2) UNIQUE NOT NULL,
     name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE subject_areas (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     -- 
     code_identifier VARCHAR(100) UNIQUE NOT NULL,
-    isbn VARCHAR(13) NOT NULL,
+    isbn VARCHAR(13) UNIQUE NOT NULL,
     issn VARCHAR(8),
     release_date DATE NOT NULL,
     subtitle VARCHAR(100),
