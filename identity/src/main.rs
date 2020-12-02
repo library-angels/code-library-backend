9a79c8e9c9eb9d624cd5bbe07fc2dfb13d09f78e
Markus Merklinger <markus.merklinger@code.berlin>
2020-12-02 11:26:25 +0100

    Removes packge name and version statics

--- identity/src/main.rs
+++ identity/src/main.rs
@@ -11,15 +11,12 @@ use identity::config::Configuration;
 use identity::db::Db;
 use identity::rpc::rpc_server;
 
-static PKG_NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
-static PKG_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
-
 #[tokio::main]
 async fn main() -> io::Result<()> {
     println!(
         "SERVICE: {} | VERSION: {}\n",
-        PKG_NAME.unwrap_or("<unknown>"),
-        PKG_VERSION.unwrap_or("<unknown>")
+        option_env!("CARGO_PKG_NAME").unwrap_or("<unknown>"),
+        option_env!("CARGO_PKG_VERSION").unwrap_or("<unknown>")
     );
     env_logger::init();
     log::info!("Starting service");
