e988f182901037b6d9735b1d6a397da0727edfa5
Johann Hemmann <johann.hemmann@code.berlin>
2020-12-06 16:00:46 +0100

    Unify `type DbPool`, `type DbConn`, `fn get_db_pool` into `helpers::db` (#181)

--- book/src/lib/db/mod.rs
+++ book/src/lib/db/mod.rs
@@ -2,13 +2,4 @@ pub(crate) mod models;
 pub(crate) mod queries;
 pub(crate) mod schema;
 
-use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
-use diesel::PgConnection;
-
-pub type DbPool = Pool<ConnectionManager<PgConnection>>;
-pub type DbConn = PooledConnection<ConnectionManager<PgConnection>>;
-
-pub fn new_db_pool(database_url: &str) -> DbPool {
-    Pool::new(ConnectionManager::new(database_url))
-        .expect("Failed creating new database connection")
-}
+pub use helpers::db::*;
