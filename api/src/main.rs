7ae3720df8e97b079c6915d6ff8a7d0031d4b0a8
Johann Hemmann <johann.hemmann@code.berlin>
2020-12-06 16:01:12 +0100

    `api`: Refactor file structure + simplify main (#182)

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,43 +1,26 @@
 use dotenv::dotenv;
 use envconfig::Envconfig;
-use std::process;
 
-use api_lib::{server, Configuration, CONFIGURATION};
-
-static PKG_NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
-static PKG_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+use api::{server, Configuration, CONFIGURATION};
 
 #[tokio::main]
 async fn main() {
     println!(
         "SERVICE: {} | VERSION: {}\n",
-        PKG_NAME.unwrap_or("<unknown>"),
-        PKG_VERSION.unwrap_or("<unknown>")
+        option_env!("CARGO_PKG_NAME").unwrap_or("<unknown>"),
+        option_env!("CARGO_PKG_VERSION").unwrap_or("<unknown>")
     );
     env_logger::init();
     log::info!("Starting service");
 
-    let configuration = {
-        dotenv().ok();
-        match Configuration::init_from_env() {
-            Ok(val) => val,
-            Err(e) => {
-                log::error!("{}", e);
-                log::error!("Terminating because of previous error.");
-                process::exit(1);
-            }
-        }
-    };
-    match CONFIGURATION.set(configuration) {
-        Ok(()) => log::info!("Successfully provided global service configuration"),
-        Err(_) => {
-            log::error!("Failed to provide global service configuration");
-            log::error!("Terminating because of previous error.");
-            process::exit(1);
-        }
-    }
+    dotenv().ok();
+    let configuration = Configuration::init_from_env().unwrap();
+    CONFIGURATION
+        .set(configuration)
+        .expect("Failed to provide global service configuration");
 
-    server()
-        .try_bind(CONFIGURATION.get().unwrap().http_socket())
-        .await;
+    let addr = CONFIGURATION.get().unwrap().http_socket();
+    let server = server().bind(addr);
+    log::info!("API Server started on {}", addr);
+    server.await
 }
