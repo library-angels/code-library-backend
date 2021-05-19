3976565aade909f1312f710ebe63705163804b42
Markus Merklinger <markus.merklinger@code.berlin>
2021-05-19 15:28:08 +0200

    Adds pagination filter to api service

---
+++ api/src/lib/filters/pagination_filter.rs
@@ -0,0 +1,30 @@
+use uuid::Uuid;
+use warp::{Filter, Rejection};
+
+use helpers::types::{Page, PageFilter};
+
+#[derive(Debug, serde::Deserialize)]
+struct PaginationFilter {
+    after: Option<Uuid>,
+    before: Option<Uuid>,
+    items: Option<i64>,
+}
+
+#[allow(dead_code)]
+pub fn pagination_filter() -> impl Filter<Extract = (PageFilter,), Error = Rejection> + Clone {
+    warp::query::<PaginationFilter>().and_then(|pagination_filter: PaginationFilter| async move {
+        if pagination_filter.after.is_some() && pagination_filter.before.is_some() {
+            return Err(warp::reject::not_found());
+        }
+
+        let page = if pagination_filter.before.is_some() {
+            Page::Before(pagination_filter.before.unwrap())
+        } else {
+            Page::After(pagination_filter.after.unwrap_or_else(Uuid::nil))
+        };
+
+        let items = pagination_filter.items.unwrap_or(10);
+
+        Ok::<PageFilter, Rejection>(PageFilter::new(page, items))
+    })
+}
