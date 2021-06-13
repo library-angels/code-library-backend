c716a75ba097cb0930890660388e4cc487c18461
Markus Merklinger <markus.merklinger@code.berlin>
2021-06-13 11:40:58 +0200

    Adds database trigger function to schema migration for automatic generating of copy ids

--- book/migrations/20210208110427_initial_schema.down.sql
+++ book/migrations/20210208110427_initial_schema.down.sql
@@ -1,3 +1,7 @@
+DROP TRIGGER copies_copy_id_create_seq ON copies CASCADE;
+DROP FUNCTION copies_copy_id_create_seq;
+DROP TRIGGER copies_copy_id_delete_seq ON copies CASCADE;
+DROP FUNCTION copies_copy_id_delete_seq;
 DROP TABLE copies;
 DROP TABLE books_tags;
 DROP TABLE books_subject_areas;
@@ -11,3 +15,4 @@ DROP TABLE publishers;
 DROP TABLE persons;
 DROP TABLE languages;
 DROP TABLE categories;
+
